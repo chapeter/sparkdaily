e644fab89a506eaa26a523a5525b71c11ab497a2
Chad Peterson <chapeter@cisco.com>
2016-07-27 13:37:30 -0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-url = "https://api.ciscospark.com/v1/"
-token = "MzI0ZmU1ZTItNjQ0YS00ZTc3LWIxNGItMjdmZWE1NDA0MTdjYjQ5Yjc0MDktYTJl"
-roomid = "Y2lzY29zcGFyazovL3VzL1JPT00vNzA5MGNmMzAtOTdhMy0xMWU1LWIyNzAtZDM2ZWRmMzJlODMz"
-ignorelist = []
-sender = "chapeter@cisco.com"
-server = "mailman.cisco.com"
-server_port = 25
